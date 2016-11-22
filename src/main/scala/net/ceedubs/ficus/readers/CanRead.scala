17df5d967513e85aa2cc7a6e352ca95bcdf9bba3
eyal farago <eyal.farago@gmail.com>
2016-11-22 23:27:17 +0200

    canRead: inroduce the CanRead trait.

---
+++ src/main/scala/net/ceedubs/ficus/readers/CanRead.scala
@@ -0,0 +1,21 @@
+package net.ceedubs.ficus.readers
+
+import com.typesafe.config.{Config, ConfigException, ConfigValue}
+
+/**
+  * Created by eyalf on 11/22/2016.
+  */
+trait CanRead[A] {
+  def canRead(config: Config, path: String) : Boolean
+
+  protected def checkReadable[T]( f : Config => String => T )(config: Config, path: String) : Boolean = {
+    try{
+      f(config)(path)
+      true
+    } catch{
+      case _ : ConfigException.WrongType | _ : ConfigException.Missing | _ : ConfigException.BadValue => false
+    }
+  }
+}
+
+trait ExtendedValueReader[A] extends ValueReader[A] with CanRead[A]
